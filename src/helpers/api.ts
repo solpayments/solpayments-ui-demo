116ca282074fe24598985676c8a91e460a05a744
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:17 +0300

    Add getMerchantAccount

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -3,7 +3,8 @@ import type { AccountInfo, Connection, ParsedAccountData } from '@solana/web3.js
 import type { Result } from './result';
 import { failure, success } from './result';
 import { SINGLE } from './constants';
-// import { MERCHANT_LAYOUT } from '../helpers/layout';
+import { MERCHANT_LAYOUT } from '../helpers/layout';
+import type { Merchant } from '../helpers/layout';
 
 interface GetProgramAccountsParams {
   connection: Connection;
@@ -13,6 +14,37 @@ interface GetProgramAccountsParams {
 type FetchProgramAccountsResult = {
   pubkey: PublicKey;
   account: AccountInfo<Buffer | ParsedAccountData>;
+};
+
+interface GetMerchantAccountParams {
+  connection: Connection;
+  ownerKey: PublicKey;
+  programId: string;
+}
+
+export const getMerchantAccount = async (
+  params: GetMerchantAccountParams
+): Promise<Result<Merchant | null>> => {
+  const { connection, ownerKey, programId } = params;
+  const programIdKey = new PublicKey(programId);
+
+  try {
+    const result = await connection.getProgramAccounts(programIdKey, {
+      commitment: SINGLE,
+      filters: [{ memcmp: { offset: 1, bytes: ownerKey.toBase58() } }],
+    });
+
+    if (result.length < 1) {
+      return success(null);
+    }
+
+    return success({
+      address: result[0].pubkey,
+      account: MERCHANT_LAYOUT.decode(result[0].account.data),
+    });
+  } catch (error) {
+    return failure(error);
+  }
 };
 
 export const fetchProgramAccounts = async (
