0a2e7dfd76e4ade9d9dd19da794d5e4d7c711853
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:37 +0300

    Add getUiAmount

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,3 +1,7 @@
 export const abbreviateAddress = (address: string): string => {
   return address.slice(0, 4) + '…' + address.slice(address.length - 4);
 };
+
+export const getUiAmount = (amount: number, decimals: number): number => {
+  return amount / 10 ** decimals;
+};
