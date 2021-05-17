5addcb4455847dc2aedb4c3c2ea700a107003105
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:22 +0300

    Add merchantStore

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,6 @@
 import { derived, writable } from 'svelte/store';
 import type { WalletAdapter } from '../helpers/types';
+import type { Merchant } from '../helpers/layout';
 
 type Adapter = WalletAdapter | undefined;
 
@@ -12,5 +13,7 @@ export const connected = derived(adapter, ($adapter) => {
   }
   return false;
 });
+/** the wallet adapter from sollet, etc */
+export const merchantStore = writable<Merchant | null>(null);
 /** the immutable program id */
 export const programId = writable<string>('8RqbzUupLSSdTGCzkZsFjUwUupWuu2Jph5x4LeU1wV7C');
