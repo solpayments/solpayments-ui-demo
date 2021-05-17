799a50520ac1536819ec7b07156d6f317a1c68e9
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:30 +0300

    Ensure addTransaction works as required

--- src/stores/transaction.ts
+++ src/stores/transaction.ts
@@ -18,18 +18,22 @@ export const transactionsMap = writable<TxMap>(new Map());
 
 /** new transaction is always added in 'Unknown' state */
 export const addTransaction = (txId: string, name: string): void => {
-  console.log('txId >>>>> ', txId);
-  console.log('name >>>>> ', name);
-  transactionsMap.update((existing) => existing.set(txId, { name, success: TxStatus.Unknown }));
+  transactionsMap.update((existing) => {
+    const possibleTx = existing.get(txId);
+    if (possibleTx) {
+      // don't update an already existing transaction
+      return existing;
+    }
+    return existing.set(txId, { name, success: TxStatus.Unknown });
+  });
 };
 
 /** update existing */
 export const updateTransaction = (txId: string, status: TxStatus): void => {
-  console.log('txId >>>>> ', txId);
-  console.log('status >>>>> ', status);
   transactionsMap.update((existing) => {
     const tx = existing.get(txId);
     if (tx) {
+      // only update an existing transaction
       existing.set(txId, { ...tx, success: status });
     }
     return existing;
