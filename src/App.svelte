d8b36e1d809e6abb9999f4401245574c82e051e1
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:23 +0300

    Remove cruft

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-  import { setContext } from 'svelte'
-  import { connectToWallet } from './helpers/wallet';
-  import { adapter, connected } from './stores';
-  import { registerMerchant } from './instructions/register';
-  import { Connection, PublicKey } from '@solana/web3.js';
-  import { SINGLE_GOSSIP } from './helpers/constants';
-  import { getMerchantAccount } from './helpers/api';
-  import { expressCheckout } from './instructions/express_checkout';
+  import { setContext } from 'svelte';
+  import { connected } from './stores';
   import Wallet from './components/Wallet.svelte';
   import MerchantComponent from './components/Merchant.svelte';
+  import ExpressCheckout from './components/Checkout.svelte';
 
   export let name: string;
-  export let programId: string;
 
   setContext('solanaNetwork', 'http://localhost:8899');
 </script>
@@ -25,57 +19,10 @@
 
   <Wallet />
 
-  <MerchantComponent />
+  {#if $connected}
+    <MerchantComponent />
 
-  {#if $connected && $adapter?.publicKey}
-    <!-- <p style="color: green">Connected to {$adapter.publicKey}</p>
-    {#await getMerchantAccount({
-      connection: new Connection('http://localhost:8899', SINGLE_GOSSIP),
-      ownerKey: $adapter.publicKey,
-      programId,
-    })}
-      <p>loading program accounts</p>
-    {:then result}
-      {console.log('>>>>>>>>>>> accounts ', result)}
-      <p style="color: green">Done</p>
-    {:catch error}
-      {console.log('errrrrrrrrrrrrrrr ?? ', error)}
-      <p style="color: red">{error}</p>
-    {/await} -->
-
-    <!-- {#await expressCheckout({
-      amount: 100000,
-      buyerTokenAccount: new PublicKey('29cG2PtMwhuN3tsGZj4yHCcVJcaBKoJAtFXw9KBuBF9V'),
-      connection: new Connection('http://localhost:8899', SINGLE_GOSSIP),
-      merchantAccount: new PublicKey('8JbSLSyLpsThT6zeXNk7CEYETtzxyBfEZQXWWuJtxrck'),
-      mint: new PublicKey('9nNBhx15F6WkT94u6uyusnWXmnxQqVro9gGdEX95Vmuu'),
-      orderId: 'order2',
-      secret: 'hunter2',
-      thisProgramId: programId,
-      wallet: $adapter,
-    })}
-      <p>loading</p>
-    {:then result}
-      {console.log('CHECKOUT >>>>>>>>>>> ', result)}
-      <p style="color: green">Done</p>
-    {:catch error}
-      {console.log('CHECKOUT errrrrrrrrrrrrrrr ?? ', error)}
-      <p style="color: red">{error}</p>
-    {/await} -->
-
-    <!-- {#await registerMerchant({
-      connection: new Connection('http://localhost:8899', SINGLE_GOSSIP),
-      thisProgramId: programId,
-      wallet: $adapter,
-    })}
-      <p>loading</p>
-    {:then result}
-      {console.log('>>>>>>>>>>> ', result)}
-      <p style="color: green">Done</p>
-    {:catch error}
-      {console.log('errrrrrrrrrrrrrrr ?? ', error)}
-      <p style="color: red">{error}</p>
-    {/await} -->
+    <ExpressCheckout />
   {:else}
     <p style="color: red">Not connected</p>
   {/if}
