07dce7f2433f270d3305486523cc3a7a618c4e5c
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-17 22:18:24 +0300

    Add getOrderAccounts

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -22,6 +22,12 @@ interface GetMerchantAccountParams {
   programId: string;
 }
 
+interface GetOrderAccountParams {
+  connection: Connection;
+  merchantKey: PublicKey;
+  programId: string;
+}
+
 export const getMerchantAccount = async (
   params: GetMerchantAccountParams
 ): Promise<Result<Merchant | null>> => {
@@ -32,6 +38,31 @@ export const getMerchantAccount = async (
     const result = await connection.getProgramAccounts(programIdKey, {
       commitment: SINGLE,
       filters: [{ memcmp: { offset: 1, bytes: ownerKey.toBase58() } }],
+    });
+
+    if (result.length < 1) {
+      return success(null);
+    }
+
+    return success({
+      address: result[0].pubkey,
+      account: MERCHANT_LAYOUT.decode(result[0].account.data),
+    });
+  } catch (error) {
+    return failure(error);
+  }
+};
+
+export const getOrderAccounts = async (
+  params: GetOrderAccountParams
+): Promise<Result<Merchant | null>> => {
+  const { connection, merchantKey, programId } = params;
+  const programIdKey = new PublicKey(programId);
+
+  try {
+    const result = await connection.getProgramAccounts(programIdKey, {
+      commitment: SINGLE,
+      filters: [{ memcmp: { offset: 3, bytes: merchantKey.toBase58() } }],
     });
 
     if (result.length < 1) {
