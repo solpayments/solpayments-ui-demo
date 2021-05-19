0909d3336972986dc442f0a58f107d23a9a07541
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-19 22:34:22 +0300

    Add data helpers

---
+++ src/helpers/data.ts
@@ -0,0 +1,16 @@
+/** Subscription package */
+export interface Package {
+  duration: number /** in seconds */;
+  name: string;
+  price: number;
+}
+
+/** Subscription packages */
+export interface Packages {
+  packages: Package[];
+}
+
+/** Used in order account data field to tie the order to a subscription */
+export interface OrderSubscription {
+  subscription: string;
+}
