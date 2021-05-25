5c74f11ba8a284750e112ead466e61ef0f2409d1
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-25 08:56:29 +0300

    Use imported CSS

--- src/App.svelte
+++ src/App.svelte
@@ -14,7 +14,9 @@
   import Orders from './components/Orders.svelte';
   import SubscriptionDemo from './demos/Subscription.svelte';
   import ShopDemo from './demos/Shop.svelte';
-
+  import './styles/normalize.css';
+  import './styles/milligram.css';
+  import './styles/solpayments.css';
   export let name: string;
   export let orderId: string;
   export let secret: string;
@@ -132,25 +134,3 @@
     <p style="color: red">Not connected</p>
   {/if}
 </main>
-
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
