559521f1e36c932dfa41bf0e3c615f1bbe1e2cda
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-26 09:28:01 +0300

    Add Home component

---
+++ src/demos/Home.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { connected } from '../stores';
+  import Wallet from '../components/Wallet/Wallet.svelte';
+  import Tokens from '../components/Tokens.svelte';
+</script>
+
+{#if !$connected}
+  <Wallet />
+{:else}
+  <h3>Tokens</h3>
+  <Tokens />
+{/if}
