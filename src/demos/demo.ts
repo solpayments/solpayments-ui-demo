536ae33e0593fd6b4c5f5131035195b57212cc21
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-27 22:15:10 +0300

    Fix formatting

--- src/demos/demo.ts
+++ src/demos/demo.ts
@@ -5,3 +5,7 @@ import type { Writable } from 'svelte/store';
 export const shopAddressStore: Writable<PublicKey | undefined> = writable<PublicKey | undefined>(
   undefined
 );
+
+export const subscriptionAddressStore: Writable<PublicKey | undefined> = writable<
+  PublicKey | undefined
+>(undefined);
