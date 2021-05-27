ea22e2c5e289f7bdc81bfd6e501d57172bce2dc2
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-27 23:17:17 +0300

    Add subscription orders demo

---
+++ src/demos/partial/SubscriptionOrders.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { derived } from 'svelte/store';
+  import { connected } from '../../stores';
+  import { merchantRegistry } from '../../stores/merchants';
+  import Wallet from '../../components/Wallet/Wallet.svelte';
+  import Orders from '../../components/Orders.svelte';
+  import Redirect from '../../components/helpers/Redirect.svelte';
+  import { subscriptionAddressStore as addressStore } from '../demo';
+
+  const merchant = derived(merchantRegistry, ($merchantRegistry) => {
+    if (addressStore && $addressStore) {
+      return $merchantRegistry.get($addressStore.toString());
+    }
+    return null;
+  });
+</script>
+
+<main class="subscription-checkout">
+  {#if $connected}
+    {#if $merchant}
+      <Orders merchantAddress={$merchant.address} />
+    {:else}
+      <Redirect to="/subscriptions" state={{ from: location }} />
+    {/if}
+  {:else}
+    <Wallet />
+  {/if}
+</main>
