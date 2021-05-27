8a8d27029a1e4efbd09bba60e6f02e287463e0d8
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-27 21:57:29 +0300

    Add demo helper store

---
+++ src/demos/demo.ts
@@ -0,0 +1,7 @@
+import type { PublicKey } from '@solana/web3.js';
+import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
+
+export const shopAddressStore: Writable<PublicKey | undefined> = writable<PublicKey | undefined>(
+  undefined
+);
