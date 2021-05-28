3d1ae062fc3bea61f38118fca0277c6793805848
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-28 09:15:51 +0300

    Clean up

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
-  import type { PublicKey } from '@solana/web3.js';
-  import { derived, writable } from 'svelte/store';
+  import { derived } from 'svelte/store';
   import { onMount } from 'svelte';
   import { links, Route, Router } from 'svelte-routing';
   import type { Packages } from './helpers/data';
   import { connected, solanaNetwork, userTokens } from './stores';
   import { getTokenRegistry } from './stores/tokenRegistry';
-  import { merchantRegistry } from './stores/merchants';
   import Button from './components/Wallet/Button.svelte';
   import Wallet from './components/Wallet/Wallet.svelte';
-  import MerchantComponent from './components/Merchant.svelte';
-  import ExpressCheckout from './components/Checkout.svelte';
-  import Subscribe from './components/Subscribe.svelte';
   import Tokens from './components/Tokens.svelte';
-  import Orders from './components/Orders.svelte';
   import SubscriptionDemo from './demos/Subscription.svelte';
   import ShopDemo from './demos/Shop.svelte';
   import ShopCheckout from './demos/partial/ShopCheckout.svelte';
@@ -21,15 +15,12 @@
   import SubscribeDemo from './demos/partial/SubscribeDemo.svelte';
   import SubscriptionOrders from './demos/partial/SubscriptionOrders.svelte';
   import Home from './demos/Home.svelte';
-  import Redirect from './components/helpers/Redirect.svelte';
   import './styles/normalize.css';
   import './styles/milligram.css';
   import './styles/solpayments.css';
-  export let name: string;
-  export let orderId: string;
-  export let secret: string;
-  export let amount: number;
+
   export let mintAddress: string;
+  export let url = '';
 
   const selectedToken = derived(userTokens, ($userTokens) => {
     const possible = $userTokens.filter(
@@ -49,39 +40,10 @@
     ],
   };
 
-  const addressStore1 = writable<PublicKey | undefined>(undefined);
-  const addressStore2 = writable<PublicKey | undefined>(undefined);
-
-  const merchant = derived(merchantRegistry, ($merchantRegistry) => {
-    if (addressStore1 && $addressStore1) {
-      return $merchantRegistry.get($addressStore1.toString());
-    }
-    return null;
-  });
-  const subscriptionMerchant = derived(merchantRegistry, ($merchantRegistry) => {
-    if (addressStore2 && $addressStore2) {
-      return $merchantRegistry.get($addressStore2.toString());
-    }
-    return null;
-  });
-  export let url = '';
-
   solanaNetwork.update(() => 'http://localhost:8899');
   onMount(async () => getTokenRegistry());
 </script>
 
-<!-- <main>
-  <Wallet />
-  <Tokens />
-  {#if $selectedToken}
-    <SubscriptionDemo
-      subscriptionName="demo2"
-      tokenAccount={$selectedToken}
-      packages={subscriptionPackages}
-    />
-    <ShopDemo tokenAccount={$selectedToken} />
-  {/if}
-</main> -->
 <Router {url}>
   <header class="header">
     <div class="container">
@@ -122,15 +84,15 @@
               ><ShopDemo><ShopOrders tokenAccount={$selectedToken} /></ShopDemo></Route
             >
             <Route path="/subscriptions">
-              <SubscriptionDemo subscriptionName="demo" packages={subscriptionPackages} />
+              <SubscriptionDemo {subscriptionName} packages={subscriptionPackages} />
             </Route>
             <Route path="/subscriptions/customer">
-              <SubscriptionDemo subscriptionName="demo" packages={subscriptionPackages}>
+              <SubscriptionDemo {subscriptionName} packages={subscriptionPackages}>
                 <SubscribeDemo tokenAccount={$selectedToken} />
               </SubscriptionDemo>
             </Route>
             <Route path="/subscriptions/orders">
-              <SubscriptionDemo subscriptionName="demo" packages={subscriptionPackages}>
+              <SubscriptionDemo {subscriptionName} packages={subscriptionPackages}>
                 <SubscriptionOrders tokenAccount={$selectedToken} />
               </SubscriptionDemo>
             </Route>
