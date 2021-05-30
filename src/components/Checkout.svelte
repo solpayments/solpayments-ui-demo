cdcd3df76fd94d6a83c66e99bdf102929d426ea9
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-05-30 23:25:47 +0300

    Allow undefined buyerToken and add support for mint

--- src/components/Checkout.svelte
+++ src/components/Checkout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onDestroy } from 'svelte';
+  import { derived } from 'svelte/store';
   import { Connection, PublicKey } from '@solana/web3.js';
   import {
     adapter,
@@ -9,6 +10,7 @@
     userTokens,
   } from '../stores';
   import type { UserToken } from '../stores';
+  import { tokenMap } from '../stores/tokenRegistry';
   import { transactionsMap, TxStatus } from '../stores/transaction';
   import { expressCheckout } from '../instructions/express_checkout';
   import { FINALIZED, PROGRAM_OWNER } from '../helpers/constants';
@@ -20,9 +22,31 @@
   let checkoutResultTxId: string | undefined = undefined;
   export let orderId: string;
   export let secret: string;
-  export let buyerToken: UserToken;
-  export let amount: number;
+  export let buyerToken: UserToken | undefined = undefined;
+  export let amount: number = 0;
   export let merchant: Merchant;
+  export let mint: PublicKey;
+  const DEFAULT_DECIMALS = 6;
+
+  const token = derived(tokenMap, ($tokenMap) => {
+    if ($tokenMap) {
+      if (mint !== undefined) {
+        return $tokenMap.get(mint.toBase58()) || null;
+      } else if (buyerToken) {
+        return $tokenMap.get(buyerToken.pubkey.toBase58()) || null;
+      }
+    }
+    return null;
+  });
+
+  $: displayAmount = amount || 0;
+  $: finalAmount = buyerToken
+    ? amount * 10 ** buyerToken.account.data.parsed.info.tokenAmount.decimals
+    : $token
+    ? amount * 10 ** $token.decimals
+    : amount * 10 ** DEFAULT_DECIMALS;
+
+  $: tokenSymbol = buyerToken ? buyerToken.symbol : $token ? $token.symbol : '';
 
   // used to ensure this store subscription does not cause mem leak
   const unsubscribe = transactionsMap.subscribe((value) => {
@@ -35,13 +59,13 @@
     checkoutProcessing = true;
     checkoutPromise = null;
     checkoutPromise =
-      $adapter && $adapter.publicKey && merchant && $userTokens.length > 0
+      $adapter && $adapter.publicKey && merchant
         ? expressCheckout({
-            amount: amount * 10 ** buyerToken.account.data.parsed.info.tokenAmount.decimals,
-            buyerTokenAccount: buyerToken.pubkey,
+            amount: finalAmount,
+            buyerTokenAccount: buyerToken?.pubkey,
             connection: new Connection($solanaNetwork, FINALIZED),
             merchantAccount: merchant.address,
-            mint: new PublicKey(buyerToken.account.data.parsed.info.mint),
+            mint,
             orderId,
             programOwnerAccount: new PublicKey(PROGRAM_OWNER),
             secret,
@@ -67,22 +91,18 @@
 
 <main>
   {#if $connected && merchant}
-    {#if buyerToken}
-      <div class="row">
-        <div class="column">
-          <input type="number" bind:value={amount} />
-          <button
-            on:click={() => handleCheckoutPromise()}
-            disabled={checkoutProcessing || checkoutPromise != null}
-          >
-            {#if checkoutProcessing || checkoutPromise != null}Processing{:else}Pay {amount.toLocaleString()}
-              {buyerToken.name} Now{/if}
-          </button>
-        </div>
+    <div class="row">
+      <div class="column">
+        <input type="number" min="0" bind:value={amount} />
+        <button
+          on:click={() => handleCheckoutPromise()}
+          disabled={checkoutProcessing || checkoutPromise != null  || !$token}
+        >
+          {#if checkoutProcessing || checkoutPromise != null}Processing{:else}Pay {displayAmount.toLocaleString()}
+            {tokenSymbol} Now{/if}
+        </button>
       </div>
-    {:else}
-      <p>select a token sir</p>
-    {/if}
+    </div>
 
     {#if checkoutPromise}
       {#await checkoutPromise}
