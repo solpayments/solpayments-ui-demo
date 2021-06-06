4730a34dedb2d003765af4c00ec3fad5338dee81
Kelvin Jayanoris <kjayanoris@gmail.com>
2021-06-06 13:05:55 +0300

    Add subscriptionPackages

--- src/demos/demo.ts
+++ src/demos/demo.ts
@@ -1,6 +1,7 @@
 import type { PublicKey } from '@solana/web3.js';
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
+import type { Package } from '../helpers/data';
 
 export const shopAddressStore: Writable<PublicKey | undefined> = writable<PublicKey | undefined>(
   undefined
@@ -9,3 +10,5 @@ export const shopAddressStore: Writable<PublicKey | undefined> = writable<Public
 export const subscriptionAddressStore: Writable<PublicKey | undefined> = writable<
   PublicKey | undefined
 >(undefined);
+
+export const subscriptionPackages: Writable<Package[]> = writable<Package[]>([]);
